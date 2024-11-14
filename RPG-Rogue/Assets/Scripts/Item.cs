8e19541dc975b4d78ad81788ba72c0e8360d7ee2
Hash'n'Slash Games <alihash24@gmail.com>
2024-11-14 08:59:15 -0500

    Began using scriptable objects

---
+++ RPG-Rogue/Assets/Scripts/Item.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName ="Item")]
+public class Item : ScriptableObject
+{
+    // List stat changes here
+    
+    // Put item sprite here
+}
