4b012bf97276388a649eff69033a8e1276dbb794
Hash'n'Slash Games <alihash24@gmail.com>
2024-11-19 12:16:09 -0500

    Created attack system
    Attack system was implemented within the code

--- RPG-Rogue/Assets/Scripts/PlayerMovement.cs
+++ RPG-Rogue/Assets/Scripts/PlayerMovement.cs
@@ -3,10 +3,14 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody2D rb;
+    Rigidbody2D rb; // Reference to player rigidbody
+    Transform attackCircle; // Empty gameobject used to create hitbox
+    LayerMask enemyLayer;
+    [SerializeField] float attackRange = 0.5f; // Attack hitbox size
     [SerializeField] float moveSpeed = 5.0f; // Dynamic movespeed
     [SerializeField] float baseMoveSpeed = 5.0f; // Base move speed; Should never change while in game
     PlayerInputActions playerAction;
+    StatHolder playerStats;
 
     Vector2 moveDirection = Vector2.zero;
     InputAction move;
@@ -17,7 +21,10 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         playerAction = new PlayerInputActions();
+        playerStats = GetComponent<StatHolder>();
         rb = GetComponent<Rigidbody2D>();
+        attackCircle = this.gameObject.transform.GetChild(0).GetComponent<Transform>();
+        enemyLayer = LayerMask.GetMask("Enemies");
     }
 
     private void OnEnable()
@@ -61,11 +68,39 @@ public class PlayerMovement : MonoBehaviour
         // Clamps speed back to the original when not sprinting
         if (!playerAction.Player.Sprint.IsPressed())
             moveSpeed = baseMoveSpeed;
+
+        // HEEEEEEEEY This is fine for testing but will need fixing once the player sprite is introduced
+        #region Player Rotation
+        if (Input.GetKey(KeyCode.RightArrow))
+            rb.MoveRotation(0f);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            rb.MoveRotation(180f);
+        if (Input.GetKey(KeyCode.UpArrow))
+            rb.MoveRotation(90f);
+        if (Input.GetKey(KeyCode.DownArrow))
+            rb.MoveRotation(-90f);
+        #endregion
     }
 
+    // Draw hit box for testing
+    void OnDrawGizmosSelected()
+    {
+        if (attackCircle != null)
+            Gizmos.DrawWireSphere(attackCircle.position, attackRange);
+    }
+
+    #region Input Action Functions
     void Attacked(InputAction.CallbackContext context)
     {
         Debug.Log("ATTACK!");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackCircle.position, attackRange, enemyLayer);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("Hit: " + enemy.name);
+            enemy.GetComponent<StatHolder>().TakeDamage(playerStats.stats.attack);
+        }
     }
 
     void Sprinted(InputAction.CallbackContext context)
@@ -78,4 +113,6 @@ public class PlayerMovement : MonoBehaviour
     {
         Debug.Log("INTERACT!");
     }
+    #endregion
+
 }
--- RPG-Rogue/Assets/Scripts/StatHolder.cs
+++ RPG-Rogue/Assets/Scripts/StatHolder.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class StatHolder : MonoBehaviour
 {
     public Statistics stats;
-
+    
     int health;
     int mana;
     int attack;
@@ -19,6 +19,37 @@ public class StatHolder : MonoBehaviour
         defense = stats.defense;
         mAttack = stats.mAttack;
         mDefense = stats.mDefense;
+    }
+
+    void Update()
+    {
+        // Clamp health to never go below 0
+        if (health <= 0) 
+        {
+            health = 0;
+        }
+    }
+
+    public void TakeDamage(int rawDamage)
+    {
+        if (health > 0)
+        {
+            int hitDamage = rawDamage - defense;
+            health -= hitDamage;
+            Debug.Log(this.gameObject.name + " has taken " + hitDamage.ToString() + " damage and has " + health.ToString() + " health left.");
+            if (health <= 0)
+            {
+                Death();
+            }
+        }
+    }
+
+    public void Death()
+    {
+        Debug.Log(this.gameObject.name + " has died!");
+        // Play a death animation
+        // Disable collider
+        // 
     }
 
 }
